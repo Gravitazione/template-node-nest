f06fba99329cbf0161c2a61c1260b473eccf73c4
SasipasCh <sasipas.chi@lightnet.io>
2024-11-17 19:16:45 +0700

    chore: update pre-commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const test = 'test'
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
